4b1d46e641e373537fdbc6a31cb33c168844e930
Ivan Kumeyko <kumeyko@hotmail.com>
2022-08-18 00:05:56 +0200

    step 4 - added buttons

--- tg_bot.py
+++ tg_bot.py
@@ -1,4 +1,5 @@
-from telegram.ext import Updater, MessageHandler, Filters
+from telegram.ext import Updater, MessageHandler, Filters, CommandHandler
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup
 import logging
 from dotenv import load_dotenv
 import os
@@ -8,6 +9,20 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
                     level=logging.INFO)
 
 logger = logging.getLogger(__name__)
+
+
+
+def start(bot, update):
+    custom_keyboard = [['Новый вопрос', 'Сдаться'],
+                       ['Мой счет']]
+    reply_markup = ReplyKeyboardMarkup(custom_keyboard)
+    
+    update.message.reply_text('Hello!', reply_markup=reply_markup)
+
+
+def dull(bot, update):
+    """Send a message when the command /start is issued."""
+    update.message.reply_text('it is Dull!')
 
 
 def echo(bot, update):
@@ -24,6 +39,12 @@ def main():
     load_dotenv()
     updater = Updater(os.getenv('TG_TOKEN'))
     dp = updater.dispatcher
+
+    # on different commands - answer in Telegram
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("dull", dull))
+
+    # on noncommand i.e message - echo the message on Telegram
     dp.add_handler(MessageHandler(Filters.text, echo))
     dp.add_error_handler(error)
     updater.start_polling()
