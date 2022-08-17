4480844cfb3a7a805c3bd8e22d50258f3dd09e91
Ivan Kumeyko <kumeyko@hotmail.com>
2022-08-17 22:44:48 +0200

    step 3 - added echo

---
+++ tg_bot.py
@@ -0,0 +1,34 @@
+from telegram.ext import Updater, MessageHandler, Filters
+import logging
+from dotenv import load_dotenv
+import os
+
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+
+def echo(bot, update):
+    """Echo the user message."""
+    update.message.reply_text(update.message.text)
+
+
+def error(bot, update, error):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', update, error)
+
+
+def main():
+    load_dotenv()
+    updater = Updater(os.getenv('TG_TOKEN'))
+    dp = updater.dispatcher
+    dp.add_handler(MessageHandler(Filters.text, echo))
+    dp.add_error_handler(error)
+    updater.start_polling()
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
