05752a7a9c0b9c7dc9f7408bf2ff8afcb6d002b8
Ivan Kumeyko <kumeyko@hotmail.com>
2022-08-18 23:51:37 +0200

    step 5

--- tg_bot.py
+++ tg_bot.py
@@ -3,6 +3,7 @@ from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup, ReplyKe
 import logging
 from dotenv import load_dotenv
 import os
+import json
 
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -11,13 +12,17 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 logger = logging.getLogger(__name__)
 
 
-
 def start(bot, update):
     custom_keyboard = [['Новый вопрос', 'Сдаться'],
                        ['Мой счет']]
     reply_markup = ReplyKeyboardMarkup(custom_keyboard)
-    
+
     update.message.reply_text('Hello!', reply_markup=reply_markup)
+
+def get_question_and_answer(file):
+    with open(file) as f:
+        dictdump = json.loads(f.read())
+        return list(dictdump.items())[0]
 
 
 def dull(bot, update):
@@ -27,7 +32,11 @@ def dull(bot, update):
 
 def echo(bot, update):
     """Echo the user message."""
-    update.message.reply_text(update.message.text)
+    if update.message.text == 'Новый вопрос':
+        quest, _ = get_question_and_answer('questions.json')
+        update.message.reply_text(quest)
+    else:
+        update.message.reply_text(update.message.text)
 
 
 def error(bot, update, error):
@@ -46,6 +55,7 @@ def main():
 
     # on noncommand i.e message - echo the message on Telegram
     dp.add_handler(MessageHandler(Filters.text, echo))
+
     dp.add_error_handler(error)
     updater.start_polling()
     updater.idle()
